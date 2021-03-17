5eccfa2f3b6cd29537c43c591342f6043a7ada4e
Jonas <jonas_elmesten@hotmail.com>
2021-03-17 11:59:05 +0100

    Adding user controller and entity.

---
+++ src/main/java/com/learndeck/User.java
@@ -0,0 +1,44 @@
+package com.learndeck;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    enum UserType {
+        STUDENT, TEACHER;
+    }
+
+    @Column(name="user_id")
+    private @Id @GeneratedValue(strategy = GenerationType.AUTO) Long userId;
+    @Column(name="user_type")
+    private String userType;
+
+    public User() {}
+
+    public User(long userId, String userType) {
+        this.userId = userId;
+        this.userType = userType;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+
+
+
+}
